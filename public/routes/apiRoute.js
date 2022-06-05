8fbc65243b28f7eb0f67585e48eebb4c34324998
Armando Ugalde <uarmando15@yahoo.com>
2022-06-05 16:50:12 -0700

    added logic for get api request

--- public/routes/apiRoute.js
+++ public/routes/apiRoute.js
@@ -1,2 +1,19 @@
-const path = require(`path`);
 const router = require(`express`).Router();
+const fs = require(`fs`);
+const path = require(`path`);
+const uuid = require(`uuid`);
+
+
+router.post(`./notes`, (req, res) => {
+    const currentNotes = fs.readFileSync(path.join(process.cwd(), `./db/db.json`));
+    const newNotes = [...currentNotes, {title: req.body.title, text: req.body.text, id: uuid()}];
+
+    fs.writeFileSync(path.join(process.cwd(), `./db/db.json`), newNotes);
+})
+
+router.get(`./notes`, (req, res) => {
+    const notes = fs.readFileSync(path.join(process.cwd(), `./db/db.json`));
+    res.json(notes);
+})
+
+module.exports = router;
