23d260416b473a85ef194ffd266d16086de22b41
Armando Ugalde <uarmando15@yahoo.com>
2022-06-05 19:25:28 -0700

    fixed bug

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(`public`));
 
-app.use(`/`, htmlRoute);
+
 app.use(`/api`, apiRoute);
+app.use(`/`, htmlRoute);
+
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
