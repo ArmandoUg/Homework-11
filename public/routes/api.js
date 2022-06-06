539917037a5a9eed2adccbf76eb64eb99b4e1e26
Armando Ugalde <uarmando15@yahoo.com>
2022-06-05 18:22:11 -0700

    2nd attempt to fix bug

--- public/routes/api.js
+++ public/routes/api.js
@@ -1,19 +1,22 @@
 const router = require(`express`).Router();
 const fs = require(`fs`);
 const path = require(`path`);
-const uuid = require(`uuid`);
+const {v4:uuid} = require(`uuid`);
 
 
 router.post(`/notes`, (req, res) => {
     const currentNotes = fs.readFileSync(path.join(process.cwd(), `/db/db.json`));
-    const newNotes = [...currentNotes, {title: req.body.title, text: req.body.text, id: uuid()}];
+    // console.log(currentNotes);
+    const newNotes = [...JSON.parse(currentNotes), {title: req.body.title, text: req.body.text, id: uuid()}];
 
-    fs.writeFileSync(path.join(process.cwd(), `/db/db.json`), newNotes);
+    fs.writeFileSync(path.join(process.cwd(), `/db/db.json`), JSON.stringify(newNotes));
+    res.send(newNotes);
 })
 
 router.get(`/notes`, (req, res) => {
     const notes = fs.readFileSync(path.join(process.cwd(), `/db/db.json`));
-    res.json(notes);
+    console.log(JSON.parse(notes));
+    res.json(JSON.parse(notes));
 })
 
 module.exports = router;
